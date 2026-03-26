951472e20301a48bb4264399be9653338d74a7a9
Saudarsh Pal <saudarsh29@gmail.com>
2026-03-26 14:44:35 +0530

    refactor(price-poller): extracting logic into startPricePoller, subscribe to btcusdt only

--- apps/price-poller-service/src/index.ts
+++ apps/price-poller-service/src/index.ts
@@ -2,7 +2,38 @@ import WebSocket from "ws";
 import { redis } from "@repo/redis"
 
 const url = "wss://stream.binance.com:9443/ws";
-const ws = new WebSocket(url);
+
+let ws : WebSocket ;
+
+const startPricePoller = () => {
+    ws = new WebSocket(url);
+
+    ws.on("open", ()=>{
+        console.log("✅ Connected to Binance");
+        const subscribeRequest = {
+            id : 1,
+            method : "SUBSCRIBE",
+            params : [ "btcusdt@bookTicker"]
+        };
+        ws.send(JSON.stringify(subscribeRequest));
+    });
+
+    ws.on("message", async(message) => {
+        try{
+            const data = JSON.parse(message.toString());
+            if(!data.s) return 
+            const envelope = JSON.stringify({ kind : "price-latest", data : data })
+            await Promise.all([
+                redis.xadd( "engine-stream", "*", "data", envelope ), 
+                redis.publish("prices",envelope),
+                redis.set(`price:${data.s.toLowerCase()}`,data.a)
+            ])
+        }catch(err){
+            console.log(err);
+        }
+    });
+
+}
 
 console.log("current status: ", redis.status)
 
@@ -14,38 +45,5 @@ redis.on('error', (err) => {
   console.error('❌ Redis Error:', err);
 });
 
-ws.on("open", ()=>{
-    const subscribeRequest = {
-        id : 1,
-        method : "SUBSCRIBE",
-        params : [ 
-            "btcusdt@bookTicker",
-            "ethusdt@bookTicker",
-            "solusdt@bookTicker"
-         ]
-    };
-    ws.send(JSON.stringify(subscribeRequest));
-})
 
-ws.on("message", async(message) => {
-    try{
-        const data = JSON.parse(message.toString());
-        if(!data.s) return 
-        const envelope = JSON.stringify({ kind : "price-latest", data : data })
-        await Promise.all([
-            redis.xadd( "engine-stream", "*", "data", envelope ), 
-            redis.publish("prices",envelope),
-            redis.set(`price:${data.s.toLowerCase()}`,data.a)
-        ])
-    }catch(err){
-        console.log(err);
-    }
-})
-
-ws.on("error",(err)=>{
-    console.log(`Binance Websocket Error: ${err}`);
-})
-
-ws.on("close",()=>{
-    console.log(`Disconnected from Binance`);
-})
+startPricePoller();
