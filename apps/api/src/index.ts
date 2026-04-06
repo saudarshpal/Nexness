0e1c3ecddeae0f7ddb7647d6556b596f5a76b644
Saudarsh Pal <saudarsh29@gmail.com>
2026-04-06 14:23:29 +0530

    fix(api): resolve CORS blocking signup and enable cookie support

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -9,7 +9,12 @@ import cookieParser from 'cookie-parser';
 const app = express();
 const PORT = process.env.PORT || 3001
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    credentials: true,               
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization',"Cookies"]
+}))
 app.use(express.json());
 app.use(cookieParser())
 
