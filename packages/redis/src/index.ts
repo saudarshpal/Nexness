c5a22f3c7e3db7895634ccd51151ca1f6bcff098
Saudarsh Pal <saudarsh29@gmail.com>
2026-05-15 16:20:17 +0530

    refactor(redis): update redis connection with redis url

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -1,8 +1,7 @@
 import Redis from 'ioredis'
 
-const host = process.env.REDIS_HOST || "127.0.0.1"
-const port = Number(process.env.REDIS_PORT) || 6379
-export const redis = new Redis ({ host, port})
+const url = process.env.REDIS_URL || "redis://localhost:6379"
+export const redis = new Redis(url)
 
 
 
